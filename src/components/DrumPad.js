edde47088ccf45df1f94e392a28d2052e870fec3
Joshua Petersen <joshpetersen@Joshuas-MacBook-Air.local>
2022-01-05 12:01:51 -0800

    initial commit

---
+++ src/components/DrumPad.js
@@ -0,0 +1,20 @@
+const DrumPad = () => {
+    return (
+        <div className="drumContainer">
+            <div className="row1">
+                <button className="pad">🥁</button>
+                <button className="pad">🥁</button>
+                <button className="pad">🥁</button>
+                <button className="pad">🥁</button>
+            </div>
+            <div className="row2">
+                <button className="pad">🥁</button>
+                <button className="pad">🥁</button>
+                <button className="pad">🥁</button>
+                <button className="pad">🥁</button>
+            </div>
+        </div>
+    )
+}
+
+export default DrumPad
