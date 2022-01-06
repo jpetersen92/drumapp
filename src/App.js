7ab1fc516a0dae919f623902879d7712f4fbe6dd
Joshua Petersen <joshpetersen@Joshuas-MacBook-Air.local>
2022-01-06 12:16:06 -0800

    started drum sound functional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import './App.css';
+import { useState } from 'react';
 import DrumPad from './components/DrumPad';
 
 function App() {
+
+  const [currentKit, setCurrentKit] = useState('Drum Kit 1');
+  const [currentPad, setCurrentPad] = useState('')
+
+  const drumKit1 = [
+  {
+    id: 'Kick',
+    url: ''
+  },
+  {
+    id: 'Snare',
+    url: ''
+  },
+  {
+    id: 'HHOP',
+    url: ''
+  },
+  {
+    id: 'HHCL',
+    url: ''
+  },
+  {
+    id: 'Tom1',
+    url: ''
+  },
+  {
+    id: 'Tom2',
+    url: ''
+  },
+  {
+    id: 'Ride',
+    url: ''
+  },
+  {
+    id: 'Crash',
+    url: ''
+  },
+]
+
   return (
     <div className='drum-machine'>
-    <DrumPad />
+    <DrumPad currentKit={currentKit} currentPad={currentPad} setCurrentPad={setCurrentPad}/>
     </div>
   );
 }
--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,9 +1,15 @@
-const DrumPad = () => {
+const DrumPad = (props) => {
+
+    const playSound = () => {
+        const soundBit = new Audio('https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3');
+        soundBit.play()
+    }
+
     return (
         <div className="drum-container">
             <div className="control">
                 <div className="screen">
-                    <h3>Drum Kit 1</h3>
+                    <h3>{props.currentKit}</h3>
                 </div>
                 <div className="controls">
                     <button className="selection">Select -</button>
@@ -16,16 +22,16 @@ const DrumPad = () => {
             </div>
             <div className="drum-pads">
             <div className="row1">
-                <button className="pad"><h4>PAD 5</h4></button>
-                <button className="pad"><h4>PAD 6</h4></button>
-                <button className="pad"><h4>PAD 7</h4></button>
-                <button className="pad"><h4>PAD 8</h4></button>
+                <button id="pad5" className="pad"><h4>PAD 5</h4></button>
+                <button id="pad6" className="pad"><h4>PAD 6</h4></button>
+                <button id="pad7" className="pad"><h4>PAD 7</h4></button>
+                <button id="pad8" className="pad"><h4>PAD 8</h4></button>
             </div>
             <div className="row2">
-                <button className="pad"><h4>PAD 1</h4></button>
-                <button className="pad"><h4>PAD 2</h4></button>
-                <button className="pad"><h4>PAD 3</h4></button>
-                <button className="pad"><h4>PAD 4</h4></button>
+                <button onClick={() => {playSound()}} id="pad1" className="pad"><h4>PAD 1</h4></button>
+                <button id="pad2" className="pad"><h4>PAD 2</h4></button>
+                <button id="pad3" className="pad"><h4>PAD 3</h4></button>
+                <button id="pad4" className="pad"><h4>PAD 4</h4></button>
             </div>
             </div>
         </div>
